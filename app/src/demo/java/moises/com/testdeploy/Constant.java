0b144d18b0a689d016c7cc3ad33c9a3b6d7ad491
Moises <moisesapaza07@gmail.com>
2017-05-18 00:21:08 -0300

    added a utility

---
+++ app/src/demo/java/moises/com/testdeploy/Constant.java
@@ -0,0 +1,11 @@
+package moises.com.testdeploy;
+
+
+public class Constant {
+
+    public enum Type{
+        DEMO, FULL;
+    }
+
+    public static final Type type = Type.DEMO;
+}
---
+++ app/src/full/java/moises/com/testdeploy/Constant.java
@@ -0,0 +1,9 @@
+package moises.com.testdeploy;
+
+public class Constant {
+    public enum Type{
+        DEMO, FULL;
+    }
+
+    public static final Type type = Type.FULL;
+}
